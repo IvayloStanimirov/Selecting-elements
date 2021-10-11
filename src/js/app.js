9d43a43b1f5e94f994fa1d94edc0c72c0e7cdffa
Ivaylo <kolevivaylo27@gmail.com>
2021-10-11 19:57:30 +0300

    Removing a card after 3 seconds

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,14 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+
+
+  let CardsNotActive = document.querySelectorAll('.card:not(.active)')
+  
+
+  setTimeout(function () {
+   for(let i = 0; i < CardsNotActive.length; i++){
+    CardsNotActive[i].style.display = 'none'
+   }
+  }, 3000);
 });
